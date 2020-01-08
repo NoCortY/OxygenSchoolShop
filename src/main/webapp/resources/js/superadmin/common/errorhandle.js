5bca809194564137d5477f4f869e9820209cc565
NoCortY <nocorty@163.com>
2020-01-08 22:42:08 +0800

    重整

--- src/main/webapp/resources/js/superadmin/common/errorhandle.js
+++
@@ -1,9 +0,0 @@
-/**
- * 
- */
-$.ajaxSetup({"error":handleError});
-function handleError(XMLHttpRequest, textStatus, errorThrown){
-	if(XMLHttpRequest.status == -1006){
-		window.top.location = root + "/SuperAdminEntrance.jsp";
-	}
-}
